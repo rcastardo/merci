c45bbaea9a8e31e9522bac47bcce094541ff080d
Reginaldo Castardo <rcastardo@gmail.com>
2014-06-27 15:38:40 -0300

    Sending e-mail

--- src/Merci/CheckoutBundle/Controller/DefaultController.php
+++ src/Merci/CheckoutBundle/Controller/DefaultController.php
@@ -8,6 +8,33 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
+        $session = $this->get('session');
+        $cart = $session->get('cart');
+        if (!$cart || $cart->count() == 0) {
+            return $this->redirect($this->generateUrl('homepage'));
+        }
         return $this->render('MerciCheckoutBundle:Default:index.html.twig');
+    }
+
+    public function successAction()
+    {
+        $session = $this->get('session');
+        $cart = $session->get('cart');
+        if (!$cart || $cart->count() == 0) {
+            return $this->redirect($this->generateUrl('homepage'));
+        }
+        $user = $this->get('security.context')->getToken()->getUser();
+        $message = \Swift_Message::newInstance()
+            ->setSubject('Merci Commerce: Pedido realizado com sucesso!')
+            ->setFrom('atendimento@merci.com')
+            ->setTo($user->getEmail())
+            ->setBody(
+                $this->renderView(
+                    'MerciCheckoutBundle:Default:email.txt.twig',
+                    array('user' => $user, 'cart' => $cart)
+                ));
+        $this->get('mailer')->send($message);
+        $session->remove('cart');
+        return $this->render('MerciCheckoutBundle:Default:success.html.twig');
     }
 }
